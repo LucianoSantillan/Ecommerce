92df2c1f031e2ae41be57e2d928d3c75efe69946
Luciano Santillan <you@example.com>
2022-08-01 00:17:10 +0200

    ui improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,142 @@
-import React from 'react';
+import React, { FC } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Button } from '@mui/material';
+import { Button, Card, FormControl, InputAdornment, InputLabel, MenuItem, OutlinedInput, Select, Typography } from '@mui/material';
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
+import GridViewIcon from '@mui/icons-material/GridView';
+import { fontWeight } from '@mui/system';
+import ActionAreaCard from './components/product';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <Button variant="contained">Hello World</Button>;
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ backgroundColor: '#ebebeb' }}>
+      <div className='container' style={{ display: 'inline-flex', margin: 'auto' }}>
+        <Filter />
+        <ProductList />
+      </div>
+
+
     </div>
   );
 }
+const FilterOptionsContainer: FC<{ children: React.ReactNode }> = ({ children }) => {
+  return <div style={{ paddingLeft: '7px' }}>{children}</div>
+}
+
+function Filter() {
+  return (
+    <Card style={{ padding: '10px', marginRight: '15px', height: 'auto' }}>
+      <div style={{ marginRight: '10px', display: 'flex', flexDirection: 'column', alignItems: 'left', textAlign: 'left' }}>
+        <FilterTitle title='Category' />
+        <FilterOptionsContainer>
+          <FilterOption title='t-shirt' />
+          <FilterOption title='pants' />
+          <FilterOption title='shoes' />
+        </FilterOptionsContainer>
+        <br />
+        <FilterTitle title='For who?' />
+        <FilterOptionsContainer>
+          <FormControl fullWidth style={{ width: '130px' }} size="small">
+            {/* <InputLabel id="demo-simple-select-label">Order by</InputLabel> */}
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={10}
+              // label="Age"
+              onChange={() => { }}
+            >
+              <MenuItem value={10}>Women</MenuItem>
+              <MenuItem value={20}>Men</MenuItem>
+            </Select>
+          </FormControl>
+        </FilterOptionsContainer>
+
+        <br />
+
+        <FilterTitle title='Price' />
+        <FilterOptionsContainer>
+          <FormControl fullWidth sx={{ m: 1, width: '100px' }} size="small">
+            <InputLabel htmlFor="outlined-adornment-amount">From</InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-amount"
+              value={100000}
+              onChange={() => { }}
+              startAdornment={<InputAdornment position="start">$</InputAdornment>}
+              label="Amount"
+            />
+          </FormControl>
+          <FormControl fullWidth sx={{ m: 1, width: '100px' }} size="small">
+            <InputLabel htmlFor="outlined-adornment-amount">To</InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-amount"
+              value={999999}
+              onChange={() => { }}
+              startAdornment={<InputAdornment position="start">$</InputAdornment>}
+              label="Amount"
+            />
+          </FormControl>
+        </FilterOptionsContainer>
+
+
+      </div>
+    </Card>
+  )
+}
+
+const FilterTitle: FC<{ title: string }> = ({ title }) => {
+  return (
+    <Typography variant='subtitle1' style={{ fontWeight: 'bold' }}>{title}</Typography>
+  )
+}
+
+const FilterOption: FC<{ title: string }> = ({ title }) => {
+  return <Typography variant='subtitle1'>{title}</Typography>
+}
+
+function ProductList() {
+  const img = { maxHeight: '230px', maxWidth: '230px', display: 'flex' }
+
+  return (
+    <div style={{ maxWidth: '940px' }}>
+      <Card style={{ marginBottom: '15px' }}>
+        <div style={{ display: 'flex', padding: '15px', alignItems: 'center' }}>
+          <div style={{ marginRight: '25px' }}>
+            <FormatListBulletedIcon style={{ marginRight: '5px' }} />
+            <GridViewIcon />
+          </div>
+          <div>40 Product found</div>
+          <div style={{ flex: 1, padding: '0 20px' }}>
+            <hr style={{ border: 'none', borderTop: '1px solid #bcccdc' }} />
+          </div>
+          <div style={{ marginRight: '5px' }}>Ordenar por</div>
+          <OrderBySelector />
+        </div>
+      </Card>
+      <div style={{ display: 'grid', gap: '5px' }}>
+        <ActionAreaCard />
+        <ActionAreaCard />
+        <ActionAreaCard />
+      </div>
+    </div >
+  )
+}
 
 export default App;
+function OrderBySelector() {
+  return <FormControl size="small">
+    {/* <InputLabel id="demo-simple-select-label">Order by</InputLabel> */}
+    <Select
+      labelId="demo-simple-select-label"
+      id="demo-simple-select"
+      value={10}
+      // label="Age"
+      onChange={() => { }}
+    >
+      <MenuItem value={10}>Ten</MenuItem>
+      <MenuItem value={20}>Twenty</MenuItem>
+      <MenuItem value={30}>Thirty</MenuItem>
+    </Select>
+  </FormControl>;
+}
+
---
+++ src/components/product.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+
+export default function ActionAreaCard() {
+  return (
+    <Card >
+      <CardActionArea style={{display: 'flex', padding: '10px'}}>
+        <CardMedia
+          component="img"
+          height="250px"
+          image="https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSMxbasiw5cWT9eJgi5j_P3lfnuDjhK-wFMy7UTrG61DSx-fDlIVkucLf--9dGPKDwxNHsOTW5fALVrv7uXEYJIOR4pxOyATLrmP5pKWj1VthcHGTSz8Lq-&usqp=CAE"
+          alt="green iguana"
+          style={{width: '250px'}}
+        />
+        <CardContent style={{textAlign: 'left'}}>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
