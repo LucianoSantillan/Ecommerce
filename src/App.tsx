684ed7d735e64abb8e42a827009ecc6d970d09a5
Luciano Santillan <you@example.com>
2022-08-23 02:28:56 +0200

    changing names

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import 'App.css';
+import { ProductDetail } from 'pages/ProductDetail/ProductDetail';
 import ProductsList from 'pages/ProductsList/ProductsList';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
 
   return (
-    <ProductsList />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ProductsList />}>
+        </Route>
+        <Route path="details" element={<ProductDetail />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,40 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import { ProductDetail } from './pages/ProductDetail/ProductDetail';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-
-        </Route>
-        <Route path="details" element={<ProductDetail />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
-
-  // <React.StrictMode>
-  //   <Router>
-  //     <Switch>
-  //       <Route path="/">
-  //         <App />
-  //       </Route>
-  //       {/* <Route path="/about">
-  //           <About />
-  //         </Route>
-  //         <Route path="/dashboard">
-  //           <Dashboard />
-  //         </Route> */}
-  //     </Switch>
-  //   </Router>
-  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
